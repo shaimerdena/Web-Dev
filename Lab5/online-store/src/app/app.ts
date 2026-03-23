a8f8a96c31dd733732c600aed486fde7e6fc7b53
Ayau <shaymerdena@gmail.com>
2026-03-23 22:48:50 +0500

    complete Lab 5 architecture

--- Lab5/online-store/src/app/app.ts
+++ Lab5/online-store/src/app/app.ts
@@ -4,6 +4,7 @@ import { ProductService } from './services/product.service';
 import { Category } from './models/category.model';
 import { Product } from './models/product.model';
 import { CommonModule } from '@angular/common';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -12,19 +13,35 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-
+export class App implements OnInit {
   categories: Category[] = [];
+  allProducts: Product[] = [];   
+  filteredProducts: Product[] = [];  
   selectedCategoryId: number | null = null;
-  selectedProducts: Product[] = [];
 
-  constructor(private productService: ProductService) {
-    this.categories = this.productService.categories;
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.categories = this.productService.getCategories();
+    this.allProducts = this.productService.getAllProducts(); 
+    this.filteredProducts = [...this.allProducts]; 
   }
-
-  selectCategory(categoryId: number) {
+  selectCategory(categoryId: number | null) {
     this.selectedCategoryId = categoryId;
-    this.selectedProducts =
-      this.productService.getProductsByCategory(categoryId);
+    
+    if (categoryId === null) {
+      this.filteredProducts = [...this.allProducts];
+    } else {
+      this.filteredProducts = this.allProducts.filter(p => p.categoryId === categoryId);
+    }
   }
+  onDelete(id: number) {
+  this.allProducts = this.allProducts.filter(p => p.id !== id);
+  
+  if (this.selectedCategoryId === null) {
+    this.filteredProducts = [...this.allProducts];
+  } else {
+    this.filteredProducts = this.allProducts.filter(p => p.categoryId === this.selectedCategoryId);
+  }
+}
 }
