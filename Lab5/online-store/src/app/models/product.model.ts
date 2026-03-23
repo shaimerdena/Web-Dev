6dddfb7ca90fa73d144fb43b8603650600cdcd0f
Ayau <shaymerdena@gmail.com>
2026-03-23 22:45:51 +0500

    feat: update models and add like/category properties

--- Lab5/online-store/src/app/models/product.model.ts
+++ Lab5/online-store/src/app/models/product.model.ts
@@ -1,12 +1,11 @@
 export interface Product {
   id: number;
+  categoryId: number;
   name: string;
   description: string;
   price: number;
   rating: number;
-  image: string;
-  images: string[];
-  link: string;
   likes: number;
-  categoryId: number;
+  image: string;
+  link: string;
 }
