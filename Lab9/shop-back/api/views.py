09c6a0cb025a895d3c7b32459015fe5b3d3006db
Ayau <shaymerdena@gmail.com>
2026-03-30 21:19:57 +0500

    refactor: replace manual API views with ModelViewSet and DefaultRouter

--- Lab9/shop-back/api/views.py
+++ Lab9/shop-back/api/views.py
@@ -1,65 +1,20 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from .models import Product, Category
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .serializers import CategorySerializer, ProductSerializer
 
-def product_list(request):
-    products = Product.objects.all()
-    products_json = [
-        {
-            'id' : p.id,
-            'name' : p.name,
-            'price' : p.price,
-            'description': p.description,
-            'count': p.count,
-            'is_active': p.is_active,
-            'category_id': p.category.id
-        } for p in products
-    ]
-    return JsonResponse(products_json, safe = False)
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-def product_detail(request, product_id):
-    try:
-        p = Product.objects.get(id=product_id)
-        return JsonResponse({
-            'id': p.id,
-            'name': p.name,
-            'price': p.price,
-            'description': p.description,
-            'count': p.count,
-            'is_active': p.is_active,
-        })
-    except Product.DoesNotExist:
-            return JsonResponse({'error': 'Product not found'}, status=404)
-    
-def category_list(request):
-    categories = Category.objects.all()
-    categories_json = [
-        {
-            'id' : cat.id,
-            'name' : cat.name,
-        } for cat in categories
-    ]
-    return JsonResponse(categories_json, safe=False)
+    @action(detail = True, methods = ['get'])
+    def products(self, request, pk = 'None'):
+        category = self.get_object()
+        products = category.products.all()
+        serializer = ProductSerializer(products, many = True)
+        return Response(serializer.data)
 
-def category_detail(request, category_id):
-    try:
-        cat = Category.objects.get(id = category_id)
-        return JsonResponse({
-            'id' : cat.id,
-            'name' : cat.name,
-        })
-    except Category.DoesNotExist:
-         return JsonResponse({'error' : 'Category not found'}, status = 404)
-
-def category_products(request, category_id):
-    try:
-        cat = Category.objects.get(id = category_id)
-        products = cat.products.all()
-        products_json = [{
-             'id' : p.id,
-             'name' : p.name,
-             'price' : p.price
-        } for p in products]
-        return JsonResponse(products_json, safe = False)
-    except Category.DoesNotExist:
-        return JsonResponse({'error': 'Category not found'}, status = 404)
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer    
