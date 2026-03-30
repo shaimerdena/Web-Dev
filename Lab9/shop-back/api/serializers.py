9e7857b338c02539e12f36b7491fafb5d72b5b50
Ayau <shaymerdena@gmail.com>
2026-03-30 21:17:34 +0500

    fix: add write_only category_id to ProductSerializer to fix IntegrityError

--- Lab9/shop-back/api/serializers.py
+++ Lab9/shop-back/api/serializers.py
@@ -8,6 +8,11 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only = True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), 
+        source='category', 
+        write_only=True
+    )
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'description', 'count', 'is_active', 'category']
+        fields = ['id', 'name', 'price', 'description', 'count', 'is_active', 'category', 'category_id']
